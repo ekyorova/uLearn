fa872da7909bab625bfe80754abd02efb149f123
GanchoDanailov <gancho.danailov@gmail.com>
2014-01-06 20:39:17 +0200

    add diplomaWorkReviews.js

---
+++ PISS.Web/App/js/uLearn/diplomaWorkReviews/diplomaWorkReviews.js
@@ -0,0 +1,58 @@
+﻿/*global angular*/
+(function (angular) {
+  'use strict';
+  angular.module('diplomaWorkReviews', [
+    'ng',
+    'ngResource',
+    'ui.router',
+    'ui.bootstrap',
+    'navigation',
+    'diplomaWorkReviews.templates',
+    'l10n',
+    'l10n-tools'
+  ]).config(['$stateProvider', function ($stateProvider) {
+    $stateProvider
+      .state({
+        name: 'diplomaWorkReviews',
+        title: 'Рцензия на дипломна работа',
+        url: '/diplomaWorkReviews',
+        parent: 'root',
+        'abstract': true
+      })
+      .state({
+        name: 'diplomaWorkReviews.search',
+        parent: 'diplomaWorkReviews',
+        url: '',
+        views: {
+          'pageView@root': {
+            templateUrl: 'uLearn/diplomaWorkReviews/templates/search.html',
+            controller: 'DiplomaWorkReviewsSearchCtrl'
+          }
+        }
+      })
+      .state({
+        name: 'diplomaWorkReviews.new',
+        title: 'Нова рецензия на дипломна работа',
+        parent: 'diplomaWorkReviews',
+        url: '/new',
+        views: {
+          'pageView@root': {
+            templateUrl: 'uLearn/diplomaWorkReviews/templates/new.html',
+            controller: 'DiplomaWorkReviewsEditCtrl'
+          }
+        }
+      })
+      .state({
+        name: 'diplomaWorkReviews.view',
+        title: 'Преглед',
+        parent: 'diplomaWorkReviews',
+        url: '/:diplomaWorkReviewId',
+        views: {
+          'pageView@root': {
+            templateUrl: 'uLearn/diplomaWorkReviews/templates/view.html',
+            controller: 'DiplomaWorkReviewsEditCtrl'
+          }
+        }
+      });
+  }]);
+}(angular));
